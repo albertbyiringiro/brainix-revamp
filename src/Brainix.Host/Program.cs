c4b8f81ce82e55c498b6da0541a6d512ecb3ca82
albertbyiringiro <albert.byiringiro@proton.me>
2026-09-16 11:24:23 +0200

    feat: add health check endpoints for brainix

--- src/Brainix.Host/Program.cs
+++ src/Brainix.Host/Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+// initialize a new instance of the webapplicationbuilder class with preconfigured defaults
 var builder = WebApplication.CreateBuilder(args);
+
+
+// add services for dependency injectino
+builder.Services.AddHealthChecks().AddCheck("self", () => HealthCheckResult.Healthy("host is up"), tags: ["live"]);
+
+// builds the web application
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapHealthChecks("/health/live", new HealthCheckOptions
+{
+    Predicate = (check) => check.Tags.Contains("live")
+});
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    Predicate = _ => true
+});
 
 app.Run();
